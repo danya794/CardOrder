2c94a21e2b4696ec3f68743bffeacd068b8398b3
Danil Kovalev <dannykvlv@gmail.com>
2023-03-09 11:42:20 +0300

    update

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.List;
 
@@ -22,6 +23,11 @@ public class CardOrderTest {
     @BeforeEach
     void setUp() {
         driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
